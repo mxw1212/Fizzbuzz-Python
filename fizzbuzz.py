5839c0f0aed0fcef901a52a37a10931b01e94087
Matthew Wheeler <mxw1212@gmail.com>
2018-03-05 18:34:30 +0000

    Fizzbuzz created from coursework

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+for number in range(0,10):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+        
+        elif number % 3 == 0:
+            print("Fizz")
+            
+        elif number % 5 == 0:
+            print("buzz")
+            
+        else:
+        print("number")
+        
